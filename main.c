bc5689bd8d1c1a3cacd88ddbc3d3ddf40a3a9ee7
Ömer Öz <ozomer005@gmail.com>
2026-05-29 18:00:54 +0300

    Kartlara gercek resimler eklendi ve gorsel arayuz tamamlandi

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h> // Isim kopyalama ve metin islemleri icin YENI EKLENDI
+#include <string.h> 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h> // RESIMLER ICIN YENI KUTUPHANE
 
 // Oyun tahtasinin boyutlari ve gorsel ayarlar
 #define SATIR 4
@@ -12,13 +13,11 @@
 #define KART_BOYUTU 100
 #define BOSLUK 20
 
-// Kartlarin arka plandaki mantigini tutacak yapi
 typedef struct {
     int id;          
     int durum;       
 } Kart;
 
-// Skor tablosu islemleri icin YENI YAPI
 typedef struct {
     char ad[100];
     int sure;
@@ -37,16 +36,33 @@ int main() {
     al_init();
     al_install_mouse(); 
     al_init_primitives_addon(); 
-    al_init_font_addon();       
+    al_init_font_addon(); 
+    al_init_image_addon(); // RESIM EKLENTISINI BASLAT
 
     ALLEGRO_DISPLAY *ekran = al_create_display(600, 600);
     al_set_window_title(ekran, "Hafiza Karti Oyunu - YZM104");
 
     ALLEGRO_FONT *font = al_create_builtin_font();
     ALLEGRO_EVENT_QUEUE *kuyruk = al_create_event_queue();
-
-    // Saniyede 1 kez tetiklenecek kronometre
     ALLEGRO_TIMER *zamanlayici = al_create_timer(1.0); 
+
+    // --- RESIMLERI BELLEGE YUKLEME ---
+    ALLEGRO_BITMAP *arkaPlan = al_load_bitmap("arka.png");
+    if (!arkaPlan) {
+        printf("HATA: 'arka.png' dosyasi bulunamadi! Lutfen klasoru kontrol et.\n");
+        return -1;
+    }
+
+    ALLEGRO_BITMAP *kartResimleri[8];
+    for (int i = 0; i < 8; i++) {
+        char dosyaAdi[20];
+        sprintf(dosyaAdi, "%d.png", i); // 0.png, 1.png diye otomatik isimlendirir
+        kartResimleri[i] = al_load_bitmap(dosyaAdi);
+        if (!kartResimleri[i]) {
+            printf("HATA: '%s' dosyasi bulunamadi!\n", dosyaAdi);
+            return -1;
+        }
+    }
 
     al_register_event_source(kuyruk, al_get_display_event_source(ekran));
     al_register_event_source(kuyruk, al_get_mouse_event_source());
@@ -56,9 +72,7 @@ int main() {
     Kart tahta[SATIR][SUTUN];
     int idListesi[16];
     
-    for (int i = 0; i < 16; i++) {
-        idListesi[i] = i / 2; 
-    }
+    for (int i = 0; i < 16; i++) idListesi[i] = i / 2; 
 
     srand(time(NULL));
     for (int i = 15; i > 0; i--) {
@@ -77,7 +91,6 @@ int main() {
         }
     }
 
-    // --- OYUN DONGUSU DEGISKENLERI ---
     bool calisiyor = true;
     int acikKartSayisi = 0;
     int ilkSatir = -1, ilkSutun = -1;
@@ -101,10 +114,16 @@ int main() {
                 int y = 60 + i * (KART_BOYUTU + BOSLUK); 
                 
                 if (tahta[i][j].durum == 0) {
-                    al_draw_filled_rectangle(x, y, x + KART_BOYUTU, y + KART_BOYUTU, al_map_rgb(100, 100, 100));
+                    // KART KAPALIYSA: arka.png resmini cizdir
+                    al_draw_scaled_bitmap(arkaPlan, 0, 0, al_get_bitmap_width(arkaPlan), al_get_bitmap_height(arkaPlan), 
+                                          x, y, KART_BOYUTU, KART_BOYUTU, 0);
                 } else {
-                    al_draw_filled_rectangle(x, y, x + KART_BOYUTU, y + KART_BOYUTU, al_map_rgb(220, 220, 220));
-                    al_draw_textf(font, al_map_rgb(0, 0, 0), x + 50, y + 45, ALLEGRO_ALIGN_CENTER, "%d", tahta[i][j].id);
+                    // KART ACIK VEYA ESLESMISSE: O karta ait numarali resmi (orn: 3.png) cizdir
+                    ALLEGRO_BITMAP *guncelResim = kartResimleri[tahta[i][j].id];
+                    al_draw_scaled_bitmap(guncelResim, 0, 0, al_get_bitmap_width(guncelResim), al_get_bitmap_height(guncelResim), 
+                                          x, y, KART_BOYUTU, KART_BOYUTU, 0);
+                    
+                  
                 }
             }
         }
@@ -161,44 +180,33 @@ int main() {
         }
     }
 
-    // --- YENI SISTEM: SKORLARI SIRALAYARAK DOSYAYA YAZMA ---
-    SkorBilgisi skorlar[100]; // En fazla 100 kayit tutalim
+    // --- SKOR TABLOSU KAYDETME VE SIRALAMA (Ayni biraktik) ---
+    SkorBilgisi skorlar[100]; 
     int skorSayisi = 0;
 
-    // 1. Eski dosyadaki skoru okuma (Varsa)
     FILE *dosya = fopen("skor_tablosu.txt", "r");
     if (dosya != NULL) {
         char satirOkuma[256];
         while (fgets(satirOkuma, sizeof(satirOkuma), dosya) && skorSayisi < 100) {
-            // "Oyuncu: Isim | Sure: X sn | Hamle Sayisi: Y" formatini parcaliyoruz
             if (sscanf(satirOkuma, "Oyuncu: %[^|]| Sure: %d sn | Hamle Sayisi: %d", 
                        skorlar[skorSayisi].ad, &skorlar[skorSayisi].sure, &skorlar[skorSayisi].hamle) == 3) {
-                
-                // Ismin sonundaki gereksiz boslugu temizleme
                 int uzunluk = strlen(skorlar[skorSayisi].ad);
-                if (uzunluk > 0 && skorlar[skorSayisi].ad[uzunluk - 1] == ' ') {
-                    skorlar[skorSayisi].ad[uzunluk - 1] = '\0';
-                }
+                if (uzunluk > 0 && skorlar[skorSayisi].ad[uzunluk - 1] == ' ') skorlar[skorSayisi].ad[uzunluk - 1] = '\0';
                 skorSayisi++;
             }
         }
         fclose(dosya);
     }
 
-    // 2. Senin az once oynayip bitirdigin yeni skoru listeye ekleme
     strcpy(skorlar[skorSayisi].ad, oyuncuAdSoyad);
     skorlar[skorSayisi].sure = gecenSure;
     skorlar[skorSayisi].hamle = hamleSayisi;
     skorSayisi++;
 
-    // 3. Siralama Algoritmasi (Bubble Sort) - Sureye gore kucukten buyuge
     for (int i = 0; i < skorSayisi - 1; i++) {
         for (int j = 0; j < skorSayisi - i - 1; j++) {
-            // Eger oncekinin suresi, sonrakinden buyukse yer degistir!
-            // (Sureler ayniysa hamle sayisi az olan uste cikar)
             if (skorlar[j].sure > skorlar[j+1].sure || 
                (skorlar[j].sure == skorlar[j+1].sure && skorlar[j].hamle > skorlar[j+1].hamle)) {
-                
                 SkorBilgisi gecici = skorlar[j];
                 skorlar[j] = skorlar[j+1];
                 skorlar[j+1] = gecici;
@@ -206,7 +214,6 @@ int main() {
         }
     }
 
-    // 4. Siralanmis listeyi dosyaya "w" (Uzerine Yazma) moduyla bastan yazdirma
     dosya = fopen("skor_tablosu.txt", "w");
     if (dosya != NULL) {
         for (int i = 0; i < skorSayisi; i++) {
@@ -217,7 +224,10 @@ int main() {
         printf("\nLiderlik tablosu guncellendi! Skorlar siralandi.\n");
     }
 
-    // --- BELLEK TEMIZLIGI ---
+    // --- BELLEK TEMIZLIGI (RESIMLERI DE SILYORUZ) ---
+    al_destroy_bitmap(arkaPlan);
+    for (int i = 0; i < 8; i++) al_destroy_bitmap(kartResimleri[i]);
+    
     al_destroy_timer(zamanlayici);
     al_destroy_display(ekran);
     al_destroy_font(font);
